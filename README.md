7bf3dd8b0df147ef6961145f095c2066af8b77b0
Arcadia Zhang <arcadia@engr2-1-8-40-edu.int.colorado.edu>
2018-02-19 13:18:09 -0700

    0: added readme and project file

---
+++ README.md
@@ -0,0 +1,1 @@
+0: added readme and project file
---
+++ getmatchdetails.py
@@ -0,0 +1,23 @@
+import requests
+import time
+import os
+
+target_skill_rating = ["NormalSkill/", "HighSkill/", "VeryHighSkill/"]
+gamemode = ["AllPick/", "CaptainsMode/"]
+lobbytype = ["Public/","SoloMid/", "CoopBot/", "Ranked/"]
+
+for lobby in lobbytype:
+    for skill in target_skill_rating:
+        for mode in gamemode:
+            dir = lobby + mode + skill + 'matchhistory/'
+            writedir = lobby + mode + skill + 'matchdetails/'
+            if not os.path.exists(writedir):
+                os.makedirs(writedir)
+            for filename in os.listdir(dir):
+                print(dir + filename)
+                time.sleep(1)
+                result = requests.get(url).json()
+                f = open(writedir + filename, 'w')
+                f.write(str(result))
+                f.close()
+
